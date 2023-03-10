f293782132300c0bb04fcc7ad7811bf171b357de
Ningw0806 <wang.ning3@northeastern.edu>
2023-03-09 21:13:19 -0800

    my exercise9

---
+++ Exercises/exercise9/src/main/java/com/example/exercise9/CalculatorApplication.java
@@ -0,0 +1,22 @@
+package com.example.exercise9;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class CalculatorApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(CalculatorApplication.class.getResource("calculator.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 300, 380);
+        stage.setTitle("Calculator");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
